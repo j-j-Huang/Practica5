8288248f09372c1975a8a22bc81f45747e6cfb9a
Huang jiajie <Huangjie1154@gmail.com>
2025-03-30 10:50:36 -0700

    subir

--- src/app.py
+++ src/app.py
@@ -1,28 +1,39 @@
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for
 from config import config
+from flask_mysqldb import MySQL
+
+from models.ModelUsers import ModelUsers
+from models.entities.users import User
 
 app = Flask(__name__)
+db = MySQL(app)
+
 
 @app.route("/")
 def index():
     return redirect("login")
 
-@app.route("/login", methods=["GET", "POST"])
-def login():
-    if request.method == "POST":
-        _user = request.form["username"]
-        _pass = request.form["password"]
-        
-        if _user == "admin" and _pass == "123":
-            return redirect(url_for("home"))
-        else:
-            return render_template("auth/login.html")
-    else:
-        return render_template("auth/login.html")
-
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+        if request.method == "POST":
+            user = User(0, request.form['username'], request.form['password'],0)
+            logged_user = ModelUsers.login(db, user)
+            if logged_user != None:
+                if logged_user.usertype == 1:
+                    return redirect(url_for("admin"))
+                else:
+                    return redirect(url_for("home"))
+            else:
+                flash("Acceso rechazado...")
+                return render_template("auth/login.html")
+        else:
+            return render_template("auth/login.html")
+
+
 
 if __name__ == '__main__':
     app.config.from_object(config['development'])
--- src/config.py
+++ src/config.py
@@ -1,3 +1,8 @@
 class DevelopmentConfig():
     DEBUG = True
+    SECRET_KEY = "qhrf$edjYTJ)*21nsThdK"
+    MYSQL_HOST = "localhost"
+    MYSQL_USER = "root"
+    MYSQL_PASSWORD = "_Taller$2020.01BD"
+    MYSQL_DB = "store"
 config = {"development": DevelopmentConfig}
