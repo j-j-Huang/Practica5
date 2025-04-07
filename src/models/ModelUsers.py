f43defded7466ba061931c39b819c9d8f1657725
Huang jiajie <Huangjie1154@gmail.com>
2025-04-07 11:46:25 -0700

    sql

--- src/models/ModelUsers.py
+++ src/models/ModelUsers.py
@@ -6,14 +6,11 @@ class ModelUsers():
     def login(self, db, user):
         try:
             cursor = db.connection.cursor()
-            cursor.execute(
-                    "call sp_verifyIdentity(%s, %s)",
-(user.username, user.password)
-                )
+            cursor.execute("call sp_verifyIdentity(%s, %s)",(user.username, user.password))
             row = cursor.fetchone()
             if row[0] != None:
                 user = User(row[0], row[1], row[2],row[4], row[3])
-                return user
+                return user 
             else:
                 return None
         except Exception as ex:
